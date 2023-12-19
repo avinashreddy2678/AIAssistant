97181e56c0ad3d2767f3a88a120d89fd95706a6e
avinash <avinashreddie777@gmail.com>
2023-12-19 12:00:30 +0530

    project completed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
     images: {
       domains: ['ai-assistant26.vercel.app'], // Add domains for images (required for external images)
       deviceSizes: [320, 420, 768, 1024, 1200], // Default device sizes for image optimization
     },
-  },
+  
 }
 
 module.exports = nextConfig;
